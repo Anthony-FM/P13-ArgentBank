6e05ddfde09fbdd01251c222f8f9222992f6771b
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-07-14 22:43:17 +0200

    Ajout des composants, du provider et du store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,24 +5,32 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 // Composants
 import Header from './components/Header';
+import Footer from './components/Footer';
 // Pages
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import UserAccount from './pages/UserAccount';
 import NotFound from './pages/NotFound';
+// store
+import { Provider } from 'react-redux';
+import store from './utils/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Header/>
-        <Routes>        
-          <Route  path="/" element={<Home/>}/>
-          <Route  path="/signin" element={<SignIn/>}/>
-          <Route  path="/user/:userId" element={<UserAccount/>}/>
-          <Route  path="*" element={<NotFound/>}/>
-        </Routes>
-    </Router>
+    <Provider store={store} >
+      <Router>
+        <Header/>
+          <Routes>        
+            <Route  path="/" element={<Home/>}/>
+            <Route  path="/signin" element={<SignIn/>}/>
+            <Route  path="/user" element={<UserAccount/>}/>
+            <Route  path="*" element={<NotFound/>}/>
+          </Routes>
+        <Footer/>
+      </Router>
+
+    </Provider>
   </React.StrictMode>
 );
 

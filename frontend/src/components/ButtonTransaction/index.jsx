1967823ff6e78cae34778b71ee14e9c856ccb55b
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-07-23 21:43:03 +0200

    ✨ création du composant ButtonTransaction

---
+++ frontend/src/components/ButtonTransaction/index.jsx
@@ -0,0 +1,7 @@
+import './index.css'
+
+export default function ButtonTransaction(){
+    return <div className="account-content-wrapper cta">
+    <button className="transaction-button">View transactions</button>
+  </div>
+}
